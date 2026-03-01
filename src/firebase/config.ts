f6986ecd6ac71e3f346c12409e18f97341362291
Wonderlightadventure Feedback <wonderlightadventurefeedback@gmail.com>
2026-03-01 18:03:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8838526620-dcc4e",
+  "appId": "1:985685794052:web:0585cd72dce680b0b28451",
+  "apiKey": "AIzaSyAvHCJ77iyX1TxFK_uwOGywPqdFUXX_HnU",
+  "authDomain": "studio-8838526620-dcc4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "985685794052"
+};
